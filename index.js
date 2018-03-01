c0d73f578a6163cb973bd27add11a579be186833
Zebulon Young <zebulonj@gmail.com>
2018-02-28 18:53:43 -0800

    Handle race between message dispatch and termination.

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ function makeBehaviorSubject( initial ) {
       data( 0, ( t, d ) => {
         if ( t === 2 ) {
           const i = sinks.indexOf( data );
-          if ( i > -1 ) sinks.splice( i, 1 );
+
+          if ( -1 < i ) {
+            sinks.splice( i, 1 );
+          }
         }
       });
 
@@ -20,8 +23,23 @@ function makeBehaviorSubject( initial ) {
         last = data;
       }
 
-      for ( let i = 0, n = sinks.length; i < n; i++ ) {
-        sinks[i]( type, data );
+      // Clone sinks (subscribers) before beginning dispatch to handle the
+      // case where a dispatched message results in a termination (unsubscribe)
+      // before all dispatches complete. Without this, mutations of `sinks`
+      // via the .splice() above were causing indexes out of range as `sinks`
+      // was iterated.
+      //
+      // I considered managing this issue by controlling concurrency (calling
+      // sinks[i] via a setTimeout( ..., 0 ) so that all notifications are
+      // dispatched before potential unsubscribes listeners), but I'm concerned
+      // that may contravene the intent and expectations of users/subscribers.
+      let subs = sinks.slice( 0 );
+
+      for ( let i = 0, n = subs.length; i < n; i++ ) {
+        // Check that the target subscriber has not been unsubscribed during iteration.
+        if ( -1 < sinks.indexOf( subs[i] ) ) {
+          subs[i]( type, data );
+        }
       }
     }
   }
